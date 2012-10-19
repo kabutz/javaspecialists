d9d05416c3d282fef9f946e062d9c20708a94755
kabutz <heinz@javaspecialists.eu>
2012-10-19 15:38:46 +0300

    Added timeout to wait of 100ms

--- src/main/java/eu/javaspecialists/tjsn/concurrency/util/FutureResult.java
+++ src/main/java/eu/javaspecialists/tjsn/concurrency/util/FutureResult.java
@@ -34,7 +34,7 @@ public class FutureResult<V> {
 
     public synchronized V get() throws InterruptedException {
         while (!ready) {
-            wait();
+            wait(100);
             System.out.println("value not ready for thread: " + Thread.currentThread());
         }
         return value;
