c658ad765382d5e6561897d7d2ee3479f5adf9bc
kabutz <heinz@javaspecialists.eu>
2013-01-11 00:12:50 +0200

    Added comment explaining how to use the AutoLock.

--- src/main/java/eu/javaspecialists/tjsn/concurrency/AutoLock.java
+++ src/main/java/eu/javaspecialists/tjsn/concurrency/AutoLock.java
@@ -21,6 +21,41 @@ package eu.javaspecialists.tjsn.concurrency;
 import java.util.concurrent.locks.*;
 
 /**
+ * The AutoLock lets us combine explicit locks with AutoCloseable.  We only
+ * have to call the static lock() method, which creates an instance of AutoLock
+ * and locks the explicit lock.  In the close() method, which is called by the
+ * try-with-resource construct, the explicit lock is unlocked again.
+ * <p/>
+ * We can use it like this:
+ * <pre>
+ *     Lock lock = new ReentrantLock();
+ *     try (AutoLock al = AutoLock.lock(lock)) {
+ *         // do something
+ *     }
+ *     // the lock is automatically unlocked here
+ * </pre>
+ * <p/>
+ * We can also lock interruptibly:
+ * <pre>
+ *     Lock lock = new ReentrantLock();
+ *     try (AutoLock al = AutoLock.lockInterruptibly(lock)) {
+ *         // do something
+ *     }
+ *     // the lock is automatically unlocked here
+ * </pre>
+ * <p/>
+ * This construct is also compatible with the ReadWriteLock.  We can lock on a
+ * read lock with the read() method and on a write lock with the write()
+ * method.  In addition, the AutoLock guards against accidental upgrading from
+ * a read lock to a write lock.  If that is attempted, instead of suspending
+ * the thread, we throw an IllegalMonitorStateException.
+ * <p/>
+ * Criticisms: It was suggested that creating objects at a time-critical place
+ * such as locking would be a bad idea.  However, in our experiments, we
+ * observed that when we ran our code with the Server HotSpot with Escape
+ * Analysis enabled, that the objects were not created on the heap.  Thus the
+ * cost of object creation was negligible, if not completely eliminated.
+ * <p/>
  * From http://www.javaspecialists.eu/archive/Issue190.html
  */
 final public class AutoLock implements AutoCloseable {
